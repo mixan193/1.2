c2f2922938ab80d7e6b1cd9256cd7439b0a512d1
Mihael Kardash <mixan193@gmail.com>
2023-06-24 23:13:32 +0300

    Добавьте файлы проекта.

---
+++ 1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
